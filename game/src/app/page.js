77de3b21ec6d65454456811d2ffd4406dd7ffb92
Aron <aronvenegas13@gmail.com>
2026-05-02 19:40:11 -0600

    Add some buttons using the new style

--- game/src/app/page.js
+++ game/src/app/page.js
@@ -4,6 +4,12 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+      <div className={styles.level_button_array}>
+        <button className={styles.level_button}>1</button>
+        <button className={styles.level_button}>2</button>
+        <button className={styles.level_button}>3</button>
+        <button className={styles.level_button}>4</button>
+      </div>
     </div>
   );
 }
