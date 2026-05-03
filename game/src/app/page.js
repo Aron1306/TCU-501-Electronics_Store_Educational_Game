641887b5a0680c2438c511ff653ad92606b0b737
Aron <aronvenegas13@gmail.com>
2026-05-02 23:53:18 -0600

    Add title to the main menu screen

--- game/src/app/page.js
+++ game/src/app/page.js
@@ -4,6 +4,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+      <h1 className={styles.title}>
+        <span className={styles.title_top}>At The</span>
+        <span className={styles.title_bottom}>Electronics Store</span>
+      </h1>
       <div className={styles.level_button_array}>
         <button className={`${styles.button} ${styles.button_level}`}></button>
         <button className={`${styles.button} ${styles.button_level}`}></button>
