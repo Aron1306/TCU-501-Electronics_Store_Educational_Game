34865ff27bda2eebe7eea93a5a1b7750c0619e15
Aron <aronvenegas13@gmail.com>
2026-05-02 23:03:51 -0600

    refactor button style to make it more reusable

--- game/src/app/page.js
+++ game/src/app/page.js
@@ -5,12 +5,12 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <div className={styles.level_button_array}>
-        <button className={styles.level_button}>1</button>
-        <button className={styles.level_button}>2</button>
-        <button className={styles.level_button}>3</button>
-        <button className={styles.level_button}>4</button>
-        <button className={styles.help_button}>?</button>
+        <button className={`${styles.button} ${styles.button_level}`}></button>
+        <button className={`${styles.button} ${styles.button_level}`}></button>
+        <button className={`${styles.button} ${styles.button_level}`}></button>
+        <button className={`${styles.button} ${styles.button_level}`}></button>
       </div>
+      <button className={`${styles.button} ${styles.button_help}`}></button>
     </div>
   );
 }
