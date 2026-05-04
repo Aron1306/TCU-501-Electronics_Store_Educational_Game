b8c6ba26baa3f1e77dfa9475888dcf2a5e68da70
Aron <aronvenegas13@gmail.com>
2026-05-04 11:40:07 -0600

    add dots to the level buttons so they look like dice

--- game/src/app/page.js
+++ game/src/app/page.js
@@ -9,10 +9,24 @@ export default function Home() {
         <span className={styles.title_bottom}>Electronics Store</span>
       </h1>
       <div className={styles.level_button_array}>
-        <button className={`${styles.button} ${styles.button_level}`}></button>
-        <button className={`${styles.button} ${styles.button_level}`}></button>
-        <button className={`${styles.button} ${styles.button_level}`}></button>
-        <button className={`${styles.button} ${styles.button_level}`}></button>
+        <button className={`${styles.button} ${styles.button_level}`}>
+          <div className={styles.dot} style={{ gridArea: "2 / 2" }}></div>
+        </button>
+        <button className={`${styles.button} ${styles.button_level}`}>
+          <div className={styles.dot} style={{ gridArea: "1 / 1" }}></div>
+          <div className={styles.dot} style={{ gridArea: "3 / 3" }}></div>
+        </button>
+        <button className={`${styles.button} ${styles.button_level}`}>
+          <div className={styles.dot} style={{ gridArea: "1 / 1" }}></div>
+          <div className={styles.dot} style={{ gridArea: "2 / 2" }}></div>
+          <div className={styles.dot} style={{ gridArea: "3 / 3" }}></div>
+        </button>
+        <button className={`${styles.button} ${styles.button_level}`}>
+          <div className={styles.dot} style={{ gridArea: "1 / 1" }}></div>
+          <div className={styles.dot} style={{ gridArea: "1 / 3" }}></div>
+          <div className={styles.dot} style={{ gridArea: "3 / 1" }}></div>
+          <div className={styles.dot} style={{ gridArea: "3 / 3" }}></div>
+        </button>
       </div>
       <button className={`${styles.button} ${styles.button_help}`}></button>
     </div>
