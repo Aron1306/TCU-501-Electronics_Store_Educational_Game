909c8fd169fe1fcc0a8418ce70b6bff44483bd84
Aron <aronvenegas13@gmail.com>
2026-05-09 18:39:24 -0600

    Add level 1 display text

---
+++ game/src/app/Dialogue.ts
@@ -0,0 +1,74 @@
+export const display_text = {
+  level1: {
+      instructions: {
+        english: [
+            "You will work as a clerk in an Electronics Store.",
+            "Your job is to give each client the device they are looking for.",
+            "Use the hints they give you to guess the device.",
+            "When you are sure of the correct device, drag it to the selection area.",
+            "If the selection is correct you will get points!",
+            "Collect the highest amount of points you can before the time runs out."
+        ],
+        spanish: [
+            "Trabajarás como dependiente en una tienda de electrónica.",
+            "Tu trabajo es darle a cada cliente el dispositivo que están buscando.",
+            "Usa las pistas que te dan para adivinar el dispositivo.",
+            "Cuando estás seguro de la respuesta correcta, arrastrala al área de selección.",
+            "¡Si la selección es correcta ganarás puntos!",
+            "Recolecta la mayor cantidad de puntos antes de que el tiempo se acabe."
+        ]
+      },
+      dialogue: [
+        {
+          text: "I need something to call my parents.",
+          answer: "Phone",
+          audio: "placeholder" /*TODO*/
+        },
+        {
+          text: "I want a big screen to watch movies.",
+          answer: "Television",
+          audio: "placeholder" /*TODO*/
+        },
+        {
+          text: "I need something to do my homework.",
+          answer: "Computer",
+          audio: "placeholder" /*TODO*/
+        },
+        {
+          text: "I want to listen to music.",
+          answer: "Headphones",
+          audio: "placeholder" /*TODO*/
+        },
+        {
+          text: "I need a small device to take photos.",
+          answer: "Camera",
+          audio: "placeholder" /*TODO*/
+        },
+        {
+          text: "I want to play video games at home.",
+          answer: "Console",
+          audio: "placeholder" /*TODO*/
+        },
+        {
+          text: "I need a big screen to read books.",
+          answer: "Tablet",
+          audio: "placeholder" /*TODO*/
+        },
+        {
+          text: "I need something to write on my computer",
+          answer: "Keyboard",
+          audio: "placeholder" /*TODO*/
+        },
+        {
+          text: "I want a computer I can take outside.",
+          answer: "Laptop",
+          audio: "placeholder" /*TODO*/
+        },
+        {
+          text: "I need my friends to hear me online",
+          answer: "Microphone",
+          audio: "placeholder" /*TODO*/
+        },
+      ]
+  }
+}
