506e40129d686830add6555c5924dea1dd1fea4d
Aron <aronvenegas13@gmail.com>
2026-05-09 18:50:43 -0600

    Add level 2 display text

--- game/src/app/Dialogue.ts
+++ game/src/app/Dialogue.ts
@@ -70,5 +70,52 @@ export const display_text = {
           audio: "placeholder" /*TODO*/
         },
       ]
-  }
+  },
+    level2: {
+      instructions: {
+        english: [
+            "You will work as a clerk in an Electronics Store.",
+            "Your job is to answer each client and help them with what they are looking for.",
+            "There will be multiple answer options bouncing and moving throughout the screen.",
+            "Use the hints they give you to select the correct answer by clicking it.",
+            "If the selection is correct you will get points!",
+            "Collect the highest amount of points you can before the time runs out."
+        ],
+        spanish: [
+            "Trabajarás como dependiente en una tienda de electrónica.",
+            "Tu trabajo es responder a cada cliente y ayudarles con lo que buscan.",
+            "Habrá múltiples opciones de respuesta rebotando y moviéndose a lo largo de la pantalla.",
+            "Usa las pistas que te dan para seleccionar la respuesta correcta clickeando.",
+            "¡Si la selección es correcta ganarás puntos!",
+            "Recolecta la mayor cantidad de puntos antes de que el tiempo se acabe."
+        ]
+      },
+      dialogue: [
+        {
+          text: "Hi, I need a yellow phone. I only want to call.",
+          answer: "We only have green phones here.",
+          audio: "placeholder" /*TODO*/
+        },
+        {
+          text: "Hello, I want a tablet for games. Not too expensive.",
+          answer: "This one is good for only 50 dollars!",
+          audio: "placeholder" /*TODO*/
+        },
+        {
+          text: "I need a laptop for school.",
+          answer: "This one is fast and good for your studies.",
+          audio: "placeholder" /*TODO*/
+        },
+        {
+          text: "Hi, I want headphones. ",
+          answer: "These are good for loud music.",
+          audio: "placeholder" /*TODO*/
+        },
+        {
+          text: "I need a really expensive computer for games.",
+          answer: " Yes, a million dollars please!",
+          audio: "placeholder" /*TODO*/
+        },
+      ]
+    }
 }
