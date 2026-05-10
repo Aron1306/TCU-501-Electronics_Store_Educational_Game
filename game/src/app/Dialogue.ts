8d0aa308c19310ad8e4e8ba9cd03c38445e7deb3
Aron <aronvenegas13@gmail.com>
2026-05-09 19:11:41 -0600

    Add level 3 and 4 display text

--- game/src/app/Dialogue.ts
+++ game/src/app/Dialogue.ts
@@ -21,57 +21,57 @@ export const display_text = {
       dialogue: [
         {
           text: "I need something to call my parents.",
-          answer: "Phone",
+          answer: "phone",
           audio: "placeholder" /*TODO*/
         },
         {
           text: "I want a big screen to watch movies.",
-          answer: "Television",
+          answer: "television",
           audio: "placeholder" /*TODO*/
         },
         {
           text: "I need something to do my homework.",
-          answer: "Computer",
+          answer: "computer",
           audio: "placeholder" /*TODO*/
         },
         {
           text: "I want to listen to music.",
-          answer: "Headphones",
+          answer: "headphones",
           audio: "placeholder" /*TODO*/
         },
         {
           text: "I need a small device to take photos.",
-          answer: "Camera",
+          answer: "camera",
           audio: "placeholder" /*TODO*/
         },
         {
           text: "I want to play video games at home.",
-          answer: "Console",
+          answer: "console",
           audio: "placeholder" /*TODO*/
         },
         {
           text: "I need a big screen to read books.",
-          answer: "Tablet",
+          answer: "tablet",
           audio: "placeholder" /*TODO*/
         },
         {
           text: "I need something to write on my computer",
-          answer: "Keyboard",
+          answer: "keyboard",
           audio: "placeholder" /*TODO*/
         },
         {
           text: "I want a computer I can take outside.",
-          answer: "Laptop",
+          answer: "laptop",
           audio: "placeholder" /*TODO*/
         },
         {
           text: "I need my friends to hear me online",
-          answer: "Microphone",
+          answer: "microphone",
           audio: "placeholder" /*TODO*/
         },
       ]
   },
-    level2: {
+  level2: {
       instructions: {
         english: [
             "You will work as a clerk in an Electronics Store.",
@@ -117,5 +117,117 @@ export const display_text = {
           audio: "placeholder" /*TODO*/
         },
       ]
-    }
+  },
+  level3: {
+      instructions: {
+        english: [
+            "You will work as a clerk in an Electronics Store, but this time virtually.",
+            "Your job is to answer each client and help them with what they are looking for.",
+            "This time, the clients will only communicate using emojis.",
+            "Use the hints they give you to guess what they are talking about and respond correctly.",
+            "You will need to select only the correct answer fragments in order to make your answer.",
+            "Each client will need a question to verify their needs and an answer to help them.",
+            "If both selections are correct you will get points!",
+            "Collect the highest amount of points you can before the time runs out."
+        ],
+        spanish: [
+            "Trabajarás como dependiente en una tienda de electrónica, pero ahora virtual.",
+            "Tu trabajo es responder a cada cliente y ayudarles con lo que buscan.",
+            "Esta vez, los clientes sólo se comunicarán con emojis.",
+            "Usa las pistas para adivinar de lo que hablan y responder correctamente.",
+            "Necesitarás seleccionar los fragmentos de la respuesta en el orden correcto para formar tu respuesta.",
+            "Cada cliente necesitará una pregunta para verificar su necesidad y una respuesta para poder ayudarles.",
+            "¡Si ambas selecciones son correctas ganarás puntos!",
+            "Recolecta la mayor cantidad de puntos antes de que el tiempo se acabe."
+        ]
+      },
+      dialogue: [
+        {
+            text: "😊📱💬❓",
+            question: "Do you want a phone for messages?",
+            answer: "This phone is good for messages."
+        },
+        {
+            text: "😊🎮💻❓",
+            question: "Do you like video games?",
+            answer: "This computer is good for games."
+        },
+        {
+            text: "😊📚📱🌙❓",
+            question: "Do you want to read at night?",
+            answer: "This tablet is good for reading."
+        },
+        {
+            text: "😊🎧🎵🔊❓",
+            question: "Do you like music?",
+            answer: "These headphones are good for music."
+        },
+        {
+            text: "😊📺🍿❓",
+            question: "Do you like watching movies?",
+            answer: "This TV is good for movies."
+        }
+      ]
+  },
+  level4: {
+    instructions: {
+      english: [
+        "You will work as a clerk in an Electronics Store.",
+        "Your job is to explain to each client what the devices are for.",
+        "The clients will show you an image of the device they are interested in.",
+        "Answer them with the correct use of the device using the options that appear on the screen.",
+        "Multiple options can be valid, drag them to the selection area if you think that's correct.",
+        "If the selection is correct you will get points!",
+        "If all the valid answers are selected then you will get even more points!",
+        "Collect the highest amount of points you can before the time runs out."
+      ],
+      spanish: [
+        "Trabajarás como dependiente en una tienda de electrónica.",
+        "Tu trabajo es explicarle a cada cliente para qué son los dispositivos.",
+        "Los clientes te mostrarán una imagen del dispositivo del que están interesados.",
+        "Respóndeles con el correcto uso del dispositivo usando las opciones que aparecen en pantalla.",
+        "Múltiples opciones pueden ser válidas, arrástralas al área de selección si crees que es correcto.",
+        "¡Si la selección es correcta ganarás puntos!",
+        "¡Si todas las opciones válidas son seleccionadas ganarás aún más puntos!",
+        "Recolecta la mayor cantidad de puntos antes de que el tiempo se acabe."
+      ]
+    },
+    dialogue: [
+      {
+        device: "phone",
+        options: [
+          "You can call people",
+          "You can send messages"
+        ]
+      },
+      {
+        device: "laptop",
+        options: [
+          "You can do homework",
+          "You can use the internet"
+        ]
+      },
+      {
+        device: "console",
+        options: [
+          "You can play video games",
+          "You can play with friends"
+        ]
+      },
+      {
+        device: "headphones",
+        options: [
+          "You can listen to music"
+        ]
+      },
+      {
+        device: "tablet",
+        options: [
+          "You can read books",
+          "You can watch videos",
+          "You can play games"
+        ]
+      }
+    ]
+  }
 }
