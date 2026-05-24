60c63c520a9c51e7e131e3a879713a1083c8c50d
Aron <aronvenegas13@gmail.com>
2026-05-23 20:26:34 -0600

    Add draggable devices and drop handling

--- game/src/app/level1/page.js
+++ game/src/app/level1/page.js
@@ -3,6 +3,10 @@ import { useEffect, useRef } from "react";
 import styles from "../page.module.css";
 
 export default function Home() {
+    const handleDrop = (e) => {
+        e.preventDefault();
+        const device = e.dataTransfer.getData("text/plain");
+  };
   return (
     <div className={`${styles.page} ${styles.page_main_menu}`} style={{ backgroundImage: "url('/image/assets/store_bg.jpg')"}}>
         <img
@@ -15,6 +19,32 @@ export default function Home() {
             className={styles.counter}
             alt="Store Counter"
         />
+        <div className={styles.customer_slot} onDrop={handleDrop} onDragOver={(e) => {e.preventDefault()}}> </div>
+        <div className={styles.device_row}>
+            <div draggable onDragStart={(e) => e.dataTransfer.setData("text/plain", "phone")}>
+                <img
+                src="/image/devices/phone.png"
+                className={styles.device}
+                />
+
+            </div>
+
+            <div draggable onDragStart={(e) => e.dataTransfer.setData("text/plain", "laptop")}>
+                <img
+                src="/image/devices/laptop.png"
+                className={styles.device}
+                />
+
+            </div>
+
+            <div draggable onDragStart={(e) => e.dataTransfer.setData("text/plain", "television")}>
+                <img
+                src="/image/devices/television.png"
+                className={styles.device}
+                />
+
+            </div>
+        </div>
     </div>
   );
 }
