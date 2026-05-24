42f621cecca389110d88cce9278fea9b7f46ac2f
Aron <aronvenegas13@gmail.com>
2026-05-23 19:18:28 -0600

    Add client and desk to level 1

--- game/src/app/level1/page.js
+++ game/src/app/level1/page.js
@@ -5,6 +5,16 @@ import styles from "../page.module.css";
 export default function Home() {
   return (
     <div className={`${styles.page} ${styles.page_main_menu}`} style={{ backgroundImage: "url('/image/assets/store_bg.jpg')"}}>
+        <img
+            src="/image/clients/man1.png"
+            className={styles.customer}
+            alt="customer"
+        />
+        <img
+            src="/image/assets/counter.png"
+            className={styles.counter}
+            alt="Store Counter"
+        />
     </div>
   );
 }
