0942f60100b4a639e32f4de3734392916dda21b8
Aron <aronvenegas13@gmail.com>
2026-05-24 12:35:52 -0600

    Add logic for random content generation like customers, devices and audio tracks

--- game/src/app/level1/page.js
+++ game/src/app/level1/page.js
@@ -1,18 +1,64 @@
 "use client";
 import { useEffect, useRef } from "react";
 import styles from "../page.module.css";
+import { display, display_text } from "../Dialogue";
 
 export default function Home() {
-    
+
     /*Handle items being dropped in the designed area*/
     const handleDrop = (e) => {
         e.preventDefault();
         const device = e.dataTransfer.getData("text/plain");
     };
+
+    const RandomDialogue = () => {
+        /* Devices array */
+        let devices_array = [null, null, null];
+   
+        /* Store random decisions */
+        let customer;
+        let audio_track;
+        let dialogue_selection;
+        let correct_device_pos;
+        let incorrect_device;
+
+        /* Decide the next dialogue */
+        dialogue_selection = display.level1.dialogue[Math.floor(Math.random() * display.level1.dialogue.length)];
+
+        /* Decide the sex of the next customer */
+        const random_customer_sex = Math.floor(Math.random() * 2);
+
+        /* Decide the next customer and their audio track */
+        if (random_customer_sex === 0){
+            customer = display.all_levels.male_customers[Math.floor(Math.random() * display.all_levels.male_customers.length)];
+            audio_track = dialogue_selection.audio_male[Math.floor(Math.random() * dialogue_selection.audio_male.length)];
+        } else {
+            customer = display.all_levels.female_customers[Math.floor(Math.random() * display.all_levels.female_customers.length)];
+            audio_track = dialogue_selection.audio_female[Math.floor(Math.random() * dialogue_selection.audio_female.length)];
+        }
+
+        /* Decide the position of the correct device in the devices array */
+        correct_device_pos = Math.floor(Math.random() * devices_array.length);
+        devices_array[correct_device_pos] = dialogue_selection.image;
+
+        /* Fill the device array with random devices, but assure there is always a correct one */
+        for (let pos = 0; pos < devices_array.length; pos++){
+            if (pos != correct_device_pos){
+                /* Avoid inserting the same image twice*/
+                do {
+                    incorrect_device = display.all_levels.devices[Math.floor(Math.random() * display.all_levels.devices.length)];
+                } while (devices_array.includes(incorrect_device));
+
+                devices_array[pos] = incorrect_device;
+            }
+        }
+
+        return {customer, audio_track, dialogue_selection, devices_array};
+    };
   return (
     <div className={`${styles.page} ${styles.page_main_menu}`} style={{ backgroundImage: "url('/image/assets/store_bg.jpg')"}}>
         <img
-            src="/image/clients/man1.png"
+            src="/image/customers/woman1.png"
             className={styles.customer}
             alt="customer"
         />
