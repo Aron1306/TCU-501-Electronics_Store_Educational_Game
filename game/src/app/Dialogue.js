03900f97dff795c5d06dca615313dc95216c1b6e
Aron <aronvenegas13@gmail.com>
2026-05-30 19:59:07 -0600

    Delete spanish instructions for all levels

--- game/src/app/Dialogue.js
+++ game/src/app/Dialogue.js
@@ -2,24 +2,14 @@ export const display = {
   level1: {
       audio_prefix: "/audio/dialogue/level1/",
       device_prefix: "/image/device/",
-      instructions: {
-        english: [
-            "You will work as a clerk in an Electronics Store.",
-            "Your job is to give each client the device they are looking for.",
-            "Use the hints they give you to guess the device.",
-            "When you are sure of the correct device, drag it to the selection area.",
-            "If the selection is correct you will get points!",
-            "Collect the highest amount of points you can before the time runs out."
-        ],
-        spanish: [
-            "Trabajarás como dependiente en una tienda de electrónica.",
-            "Tu trabajo es darle a cada cliente el dispositivo que están buscando.",
-            "Usa las pistas que te dan para adivinar el dispositivo.",
-            "Cuando estás seguro de la respuesta correcta, arrastrala al área de selección.",
-            "¡Si la selección es correcta ganarás puntos!",
-            "Recolecta la mayor cantidad de puntos antes de que el tiempo se acabe."
-        ]
-      },
+      instructions: [
+        "You will work as a clerk in an Electronics Store.",
+        "Your job is to give each client the device they are looking for.",
+        "Use the hints they give you to guess the device.",
+        "When you are sure of the correct device, drag it to the selection area.",
+        "If the selection is correct you will get points!",
+        "Collect the highest amount of points you can before the time runs out."
+      ],
       dialogue: [
         {
           text: "I need something to call my parents.",
@@ -95,24 +85,14 @@ export const display = {
   },
   level2: {
       audio_prefix: "/audio/dialogue/level2/",
-      instructions: {
-        english: [
-            "You will work as a clerk in an Electronics Store.",
-            "Your job is to answer each client and help them with what they are looking for.",
-            "There will be multiple answer options bouncing and moving throughout the screen.",
-            "Use the hints they give you to select the correct answer by clicking it.",
-            "If the selection is correct you will get points!",
-            "Collect the highest amount of points you can before the time runs out."
-        ],
-        spanish: [
-            "Trabajarás como dependiente en una tienda de electrónica.",
-            "Tu trabajo es responder a cada cliente y ayudarles con lo que buscan.",
-            "Habrá múltiples opciones de respuesta rebotando y moviéndose a lo largo de la pantalla.",
-            "Usa las pistas que te dan para seleccionar la respuesta correcta clickeando.",
-            "¡Si la selección es correcta ganarás puntos!",
-            "Recolecta la mayor cantidad de puntos antes de que el tiempo se acabe."
-        ]
-      },
+      instructions: [
+        "You will work as a clerk in an Electronics Store.",
+        "Your job is to answer each client and help them with what they are looking for.",
+        "There will be multiple answer options bouncing and moving throughout the screen.",
+        "Use the hints they give you to select the correct answer by clicking it.",
+        "If the selection is correct you will get points!",
+        "Collect the highest amount of points you can before the time runs out."
+      ],
       dialogue: [
         {
           text: "Hi, I need a yellow phone. I only want to call.",
@@ -152,28 +132,16 @@ export const display = {
       ]
   },
   level3: {
-      instructions: {
-        english: [
-            "You will work as a clerk in an Electronics Store, but this time virtually.",
-            "Your job is to answer each client and help them with what they are looking for.",
-            "This time, the clients will only communicate using emojis.",
-            "Use the hints they give you to guess what they are talking about and respond correctly.",
-            "You will need to select only the correct answer fragments in order to make your answer.",
-            "Each client will need a question to verify their needs and an answer to help them.",
-            "If both selections are correct you will get points!",
-            "Collect the highest amount of points you can before the time runs out."
-        ],
-        spanish: [
-            "Trabajarás como dependiente en una tienda de electrónica, pero ahora virtual.",
-            "Tu trabajo es responder a cada cliente y ayudarles con lo que buscan.",
-            "Esta vez, los clientes sólo se comunicarán con emojis.",
-            "Usa las pistas para adivinar de lo que hablan y responder correctamente.",
-            "Necesitarás seleccionar los fragmentos de la respuesta en el orden correcto para formar tu respuesta.",
-            "Cada cliente necesitará una pregunta para verificar su necesidad y una respuesta para poder ayudarles.",
-            "¡Si ambas selecciones son correctas ganarás puntos!",
-            "Recolecta la mayor cantidad de puntos antes de que el tiempo se acabe."
-        ]
-      },
+      instructions: [
+        "You will work as a clerk in an Electronics Store, but this time virtually.",
+        "Your job is to answer each client and help them with what they are looking for.",
+        "This time, the clients will only communicate using emojis.",
+        "Use the hints they give you to guess what they are talking about and respond correctly.",
+        "You will need to select only the correct answer fragments in order to make your answer.",
+        "Each client will need a question to verify their needs and an answer to help them.",
+        "If both selections are correct you will get points!",
+        "Collect the highest amount of points you can before the time runs out."
+      ],
       dialogue: [
         {
             text: "😊📱💬❓",
@@ -205,28 +173,16 @@ export const display = {
   level4: {
     audio_prefix: "/audio/dialogue/level4/",
     device_prefix: "/image/device/",
-    instructions: {
-      english: [
-        "You will work as a clerk in an Electronics Store.",
-        "Your job is to explain to each client what the devices are for.",
-        "The clients will show you an image of the device they are interested in.",
-        "Answer them with the correct use of the device using the options that appear on the screen.",
-        "Multiple options can be valid, drag them to the selection area if you think that's correct.",
-        "If the selection is correct you will get points!",
-        "If all the valid answers are selected then you will get even more points!",
-        "Collect the highest amount of points you can before the time runs out."
-      ],
-      spanish: [
-        "Trabajarás como dependiente en una tienda de electrónica.",
-        "Tu trabajo es explicarle a cada cliente para qué son los dispositivos.",
-        "Los clientes te mostrarán una imagen del dispositivo del que están interesados.",
-        "Respóndeles con el correcto uso del dispositivo usando las opciones que aparecen en pantalla.",
-        "Múltiples opciones pueden ser válidas, arrástralas al área de selección si crees que es correcto.",
-        "¡Si la selección es correcta ganarás puntos!",
-        "¡Si todas las opciones válidas son seleccionadas ganarás aún más puntos!",
-        "Recolecta la mayor cantidad de puntos antes de que el tiempo se acabe."
-      ]
-    },
+    instructions: [
+      "You will work as a clerk in an Electronics Store.",
+      "Your job is to explain to each client what the devices are for.",
+      "The clients will show you an image of the device they are interested in.",
+      "Answer them with the correct use of the device using the options that appear on the screen.",
+      "Multiple options can be valid, drag them to the selection area if you think that's correct.",
+      "If the selection is correct you will get points!",
+      "If all the valid answers are selected then you will get even more points!",
+      "Collect the highest amount of points you can before the time runs out."
+    ],
     dialogue: [
       {
         device: "phone",
